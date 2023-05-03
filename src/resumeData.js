282fce016b086a125e0a303b14aefbc20d3bb7c1
BJORN LINDQVIST <Bclindqvist@gmail.com>
2023-05-02 17:47:19 -1000

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+  imagebaseurl: "https://bjornclind.github.io/",
+  name: "Bjorn Lindqvist",
+  role: "Full Stack Developer & Software Engineer",
+  linkedinId: "https://www.linkedin.com/in/bclindqvist/",
+  roleDescription:
+    " With a passion for futurology and Star Trek, I truly believe that technology can make a big difference in people's lives. I'm committed to making that happen by using my skills to create useful and accessible solutions. Whether it's developing a website or building an app, my goal is to create something that makes someones day-to-day tasks easier and more efficient. So if you're looking for a tech-savvy problem solver, I'm your person!",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/bclindqvist/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/BjornCLind",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "I am a freelance agent working on personal projects and dabbling in ML & Python at the moment. I have a passion for learning and technology ",
+  address: "Hawaii, USA",
+  website: "https://github.com/BjornCLind",
+  education: [
+    {
+      UniversityName: "University of Hawaii Maui College",
+      specialization: "BAS in Information Technology",
+      GradMonth: "May",
+      GradYear: "2021",
+      Achievements:
+        "Lead IT Team for a startup non-profit FoodBank during COVID lockdown",
+    },
+    {
+      UniversityName: "MIT",
+      specialization: "Full Stack Certification",
+      GradMonth: "May",
+      GradYear: "2022",
+      Achievements:
+        "Professional Certificate in Coding: Full Stack Development with MERN",
+    },
+  ],
+  /*"work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    */
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "NodeJS",
+    },
+    {
+      skillname: "SQL",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Moving Eyes",
+      description: "Mouse tracker",
+      imgurl: "../images/eyes.PNG",
+      link: "https://bjornclind.github.io/Eyes/",
+    },
+    {
+      name: "Infinite Pacmen",
+      description: "Object boundry recognition",
+      imgurl: "../images/pacman.PNG",
+      link: "https://bjornclind.github.io/PacMen/",
+    },
+    {
+      name: "Bus Stops",
+      description: "Mapbox API, Location Animation",
+      imgurl: "../images/busstops.PNG",
+      link: "https://bjornclind.github.io/BusStops/",
+    },
+    {
+      name: "Site Connectivity Checker",
+      description: "Python App",
+      imgurl: "../images/testimonials-bg.jpg",
+      link: "https://bjornclind.github.io/Eyes/",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+  
+  export default resumeData
